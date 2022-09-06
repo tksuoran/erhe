b1046531b3121aa8be2dc6ef09702a414cb8a4e4
Timo Suoranta <tksuoran@gmail.com>
2022-09-06 19:41:48 +0300

    Use libfmt tag instead of origin/master

--- cmake/libraries.cmake
+++ cmake/libraries.cmake
@@ -54,8 +54,8 @@ FetchContent_Declare(
 FetchContent_Declare(
     fmt
     GIT_REPOSITORY https://github.com/fmtlib/fmt.git
-    GIT_TAG        origin/master
-    #GIT_TAG        8.1.1
+    #GIT_TAG        origin/master
+    GIT_TAG        9.1.0
     GIT_SHALLOW    TRUE
     GIT_PROGRESS   TRUE
 )
