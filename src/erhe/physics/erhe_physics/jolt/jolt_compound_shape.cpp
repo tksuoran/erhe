4eba48d3de8115a1452a08a6263acc9e1bc726b8
Timo Suoranta <tksuoran@gmail.com>
2023-11-21 10:48:08 +0200

    Jolt fix

--- src/erhe/physics/erhe_physics/jolt/jolt_compound_shape.cpp
+++ src/erhe/physics/erhe_physics/jolt/jolt_compound_shape.cpp
@@ -18,37 +18,22 @@ auto ICollision_shape::create_compound_shape_shared(
     return std::make_shared<Jolt_compound_shape>(create_info);
 }
 
-namespace
-{
-
-auto make_static_compound_shape_settings(
+Jolt_compound_shape::Jolt_compound_shape(
     const Compound_shape_create_info& create_info
-) -> JPH::StaticCompoundShapeSettings
+)
 {
-    JPH::StaticCompoundShapeSettings shape_settings;
+    m_shape_settings = new JPH::StaticCompoundShapeSettings();
     for (const auto& entry : create_info.children) {
         std::shared_ptr<ICollision_shape>     icollision_shape = entry.shape;
         std::shared_ptr<Jolt_collision_shape> jolt_shape       = std::static_pointer_cast<Jolt_collision_shape>(icollision_shape);
         JPH::ShapeRefC                        shape_ref        = jolt_shape->get_jolt_shape();
         const auto basis = glm::quat{entry.transform.basis};
-        shape_settings.AddShape(
+        m_shape_settings->AddShape(
             to_jolt(entry.transform.origin),
             to_jolt(basis),
             shape_ref
         );
     }
-    return shape_settings;
-}
-
-}
-
-Jolt_compound_shape::Jolt_compound_shape(
-    const Compound_shape_create_info& create_info
-)
-{
-    m_shape_settings = new JPH::StaticCompoundShapeSettings(
-        make_static_compound_shape_settings(create_info)
-    );
     const auto result = m_shape_settings->Create();
     ERHE_VERIFY(result.IsValid());
     m_jolt_shape = result.Get();
