9e05dfdedfacd3d058b8c820561c60b0e0526c0a
Timo Suoranta <tksuoran@gmail.com>
2025-05-13 17:07:24 +0300

    Tweak message bus locking

--- src/erhe/message_bus/erhe_message_bus/message_bus.hpp
+++ src/erhe/message_bus/erhe_message_bus/message_bus.hpp
@@ -17,13 +17,13 @@ class Message_bus
 public:
     void add_receiver(std::function<void(Message_type&)> message_receiver)
     {
-        std::lock_guard<ERHE_PROFILE_LOCKABLE_BASE(std::mutex)> lock{m_mutex};
+        std::lock_guard<ERHE_PROFILE_LOCKABLE_BASE(std::mutex)> lock{m_messages_mutex};
         m_receivers.push_back(message_receiver);
     }
 
     void send_message(Message_type message)
     {
-        std::lock_guard<ERHE_PROFILE_LOCKABLE_BASE(std::mutex)> lock{m_mutex};
+        std::lock_guard<ERHE_PROFILE_LOCKABLE_BASE(std::mutex)> lock{m_receivers_mutex};
         for (auto iter = m_receivers.begin(); iter != m_receivers.end(); iter++) {
             (*iter)(message);
         }
@@ -31,13 +31,14 @@ public:
 
     void queue_message(Message_type message)
     {
-        std::lock_guard<ERHE_PROFILE_LOCKABLE_BASE(std::mutex)> lock{m_mutex};
+        std::lock_guard<ERHE_PROFILE_LOCKABLE_BASE(std::mutex)> lock{m_messages_mutex};
         m_messages.push(message);
     }
 
     void update()
     {
-        std::lock_guard<ERHE_PROFILE_LOCKABLE_BASE(std::mutex)> lock{m_mutex};
+        std::lock_guard<ERHE_PROFILE_LOCKABLE_BASE(std::mutex)> lock_1{m_receivers_mutex};
+        std::lock_guard<ERHE_PROFILE_LOCKABLE_BASE(std::mutex)> lock_2{m_messages_mutex};
         while (!m_messages.empty()) {
             for (auto iter = m_receivers.begin(); iter != m_receivers.end(); iter++) {
                 (*iter)(m_messages.front());
@@ -47,8 +48,10 @@ public:
     }
 
 private:
-    ERHE_PROFILE_MUTEX(std::mutex,                   m_mutex);
+    ERHE_PROFILE_MUTEX(std::mutex,                   m_receivers_mutex);
     std::vector<std::function<void (Message_type&)>> m_receivers;
+
+    ERHE_PROFILE_MUTEX(std::mutex,                   m_messages_mutex);
     std::queue<Message_type>                         m_messages;
 };
 
