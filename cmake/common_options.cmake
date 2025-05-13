57e803bb8baa90c219c53a897621f94f474f5f58
Timo Suoranta <tksuoran@gmail.com>
2025-05-13 21:39:27 +0300

    Add PCH for linux

--- cmake/common_options.cmake
+++ cmake/common_options.cmake
@@ -6,17 +6,12 @@ add_definitions(-DGLM_FORCE_DEPTH_ZERO_TO_ONE)
 add_definitions(-DFMT_HEADER_ONLY)
 add_definitions(-DERHE_DLOAD_ALL_GL_SYMBOLS)
 
-find_package(Threads REQUIRED)
-
 function (erhe_target_settings target foldername)
     erhe_target_settings_toolchain(${target})
 
     if(NOT "${target}" STREQUAL "erhe_pch")
         if (${ERHE_USE_PRECOMPILED_HEADERS})
             target_precompile_headers(${target} REUSE_FROM erhe_pch)
-
-            # Avoid "POSIX thread support was disabled in AST file"
-            target_link_libraries(${target} PRIVATE Threads::Threads)
 
             # Distribute compile defines defines from spdlog
             target_link_libraries(${target} PUBLIC erhe_pch)
