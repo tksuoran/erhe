b0e098db32b909c3acb7990d2ee4fec0aa84e00b
Timo Suoranta <tksuoran@gmail.com>
2025-06-30 10:25:06 +0300

    Workaround for triangle/gcc 15

--- cmake/GNU.cmake
+++ cmake/GNU.cmake
@@ -7,6 +7,9 @@ add_compile_options("$<$<COMPILE_LANGUAGE:CXX>:-Wno-empty-body>")
 add_compile_options("$<$<CONFIG:RELEASE>:-O3>")
 add_compile_options("$<$<CONFIG:DEBUG>:-O0;-g3>")
 
+# Workaround for https://github.com/BrunoLevy/geogram/issues/257
+add_compile_options("$<$<COMPILE_LANGUAGE:C>:-std=gnu99>")
+
 # Workaround for MinGW linker (ld) is failing due to relocation overflows 
 if (WIN32)
     add_compile_options(-Wa,-mbig-obj)
